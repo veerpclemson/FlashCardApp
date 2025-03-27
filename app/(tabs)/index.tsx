c5f9680650e9a729369f4744589a51c5bc955197
veerpclemson <veerp@clemson.edu>
2025-03-27 16:12:43 -0400

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Text, View, StyleSheet } from 'react-native';
+ import { Link } from 'expo-router'; 
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/create" style={styles.button}>
+        Go to create screen
+      </Link>
+      <Link href="/view" style={styles.button}>
+        Go to view screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
---
+++ app/(tabs)/view.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function AboutScreen() {
+  const [buttonCount, setButtonCount] = useState(4); // Default to 2 buttons
+  const [buttonTopPositions, setButtonTopPositions] = useState([20]); 
+  const padding = 20; // Padding between buttons
+
+  
+  const handleLayout = (event: any, index: number) => {
+    const { height } = event.nativeEvent.layout;
+    setButtonTopPositions((prevPositions) => {
+      const newPositions = [...prevPositions];
+      const nextTop = newPositions[index] + height + padding;
+      newPositions.push(nextTop);
+      return newPositions;
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      {Array.from({ length: buttonCount }).map((_, index) => (
+        <Link
+          key={index}
+          href={`../sets/testset${index + 1}`}
+          style={[styles.button, { top: buttonTopPositions[index] }]}
+          onLayout={(event) => handleLayout(event, index)}
+        >
+          <Text style={styles.buttonText}>{`Test Set ${index + 1}`}</Text>
+        </Link>
+      ))}
+
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    paddingTop: 20,
+    justifyContent: 'flex-start', // Align items at the top
+    paddingHorizontal: 20, // Horizontal padding for better alignment
+  },
+  button: {
+    position: 'absolute',
+    width: 400,
+    height: 240,
+    left: 20,
+    backgroundColor: '#fff',
+    borderWidth: 5,
+    borderColor: '#000',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 18,
+  },
+});
